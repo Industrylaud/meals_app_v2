345fd72e38cf32a0be22c0b0eee293caeaabb448
Industrylaud <mrfros22@gmail.com>
2023-04-16 17:08:53 +0200

    add category item with model

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
